359bfbf12a4d9dff320e9bfcd6da82729decb34f
yogesh <yogesh.paisode@gmai.com>
2019-06-01 17:26:43 +0530

    Bootom bar

--- lib/main.dart
+++ lib/main.dart
@@ -9,21 +9,47 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.lightGreen,
-      ),
       home: _HomeScreen(),
     );
   }
 }
 
 class _HomeScreen extends StatelessWidget {
+  final appColor = Color(0xFF4da361);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.amber,
       appBar: AppBar(
         title: Text("Organo"),
+        backgroundColor: appColor,
       ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+
+        floatingActionButton: FloatingActionButton(
+          child: Text("2"),
+          backgroundColor: Colors.amberAccent,
+        ),
+        bottomNavigationBar: BottomAppBar(
+          shape: CircularNotchedRectangle(),
+          color: appColor,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.menu),
+                color: Colors.white,
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.search),
+                color: Colors.white,
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ) ,
       body: ListView(
         physics: BouncingScrollPhysics(),
         children: <Widget>[
